5e04382a2b721d9b923fec28ee11fb0c05c297e0
sagarb2003 <sagarssb2003@gmail.com>
2024-08-14 13:06:14 +0530

    feat:integrated gemini api and next js setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,121 @@
-import Image from "next/image";
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+
+interface ChatMessage {
+  role: "user" | "aiResponse";
+  text: string;
+}
 
 export default function Home() {
+  const [userQuestion, setUserQuestion] = useState<string>("");
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  const [loader, setLoader] = useState<boolean>(false);
+  const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
+  async function generateButtonHandler() {
+    if (!userQuestion?.trim()) return;
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { role: "user", text: userQuestion },
+    ]);
+    setLoader(true);
+
+    try {
+      const response = await axios({
+        url: `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${process.env.NEXT_PUBLIC_API_KEY}`,
+        method: "POST",
+        data: {
+          contents: [{ role: "user", parts: [{ text: userQuestion }] }],
+        },
+      });
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        {
+          role: "aiResponse",
+          text: response.data.candidates[0].content.parts[0].text,
+        },
+      ]);
+    } catch (error) {
+      console.error("Error generating response:", error);
+    } finally {
+      setLoader(false);
+      setUserQuestion("");
+    }
+  }
+
+  const truncateText = (text: string) => {
+    const words = text.split(" ");
+    if (words.length > 50) {
+      return words.slice(0, 50).join(" ") + "...";
+    }
+    return text;
+  };
+  console.log(process.env.API_KEY);
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="flex flex-col items-center justify-center mt-4">
+        <h1 className="font-bold text-2xl mb-2">Let's Chat with AI</h1>
+        <div className="flex items-center space-x-2 w-full max-w-3xl">
+          <input
+            type="text"
+            value={userQuestion}
+            onChange={(e) => setUserQuestion(e.target.value)}
+            className="h-12 flex-grow border border-black rounded-md p-2"
+            placeholder="Message here"
+          />
+          <p
+            onClick={generateButtonHandler}
+            className="text-5xl cursor-pointer bg-slate-600 pl-1 text-white w-12 h-12 rounded-full"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            ↑
+          </p>
         </div>
+
+        {loader && (
+          <p className="text-lg font-semibold mt-2 text-gray-700 animate-pulse">
+            Loading...
+          </p>
+        )}
+        {messages.length > 0 && (
+          <div className="mt-4 mb-4 w-5/6 p-4 border border-black bg-white rounded-md">
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={`my-2 p-2 rounded-md ${
+                  message.role === "user"
+                    ? "bg-blue-100 text-blue-900"
+                    : "bg-gray-100 text-gray-900"
+                }`}
+              >
+                {message.role === "aiResponse" ? (
+                  <div>
+                    {expandedIndex === index
+                      ? message.text
+                      : truncateText(message.text)}
+                    {message.text.length > 50 && (
+                      <button
+                        onClick={() => {
+                          expandedIndex === index
+                            ? setExpandedIndex(null)
+                            : setExpandedIndex(index);
+                        }}
+                        className="text-blue-600 ml-2"
+                      >
+                        {expandedIndex === index ? "Read Less" : "Read More"}
+                      </button>
+                    )}
+                  </div>
+                ) : (
+                  message.text
+                )}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
